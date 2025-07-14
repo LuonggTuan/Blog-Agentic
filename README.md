b8abbe8cf41e01c55d543d34681a8012a79cdde2
LuonggTuan <tuan.dilligaf@gmail.com>
2025-07-14 16:19:17 +0700

    Blog Agentic

---
+++ README.md
@@ -0,0 +1,5 @@
+# End to End Blog Agentic Project
+
+## Run app with langgraph cli
+```bash
+langgraph dev
--- src/graphs/graph_builder.py
+++ src/graphs/graph_builder.py
@@ -71,4 +71,5 @@ llm = GroqLLM().get_llm()
 
 ## Get the graph
 graph_builder = GraphBuilder(llm)
-graph = graph_builder.build_language_graph().compile()
+graph = graph_builder.build_language_graph().compile() #simple graph for language
+#graph = graph_builder.build_topic_graph().compile() #simple graph for topic
